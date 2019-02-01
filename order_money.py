54ce6e6ee0a58ee5931a865530de6e950ddb8746
tujinliangtester <tujinliangtester@163.com>
2019-02-01 11:16:41 +0800

    order money

--- order_money.py
+++ order_money.py
@@ -10,9 +10,6 @@ product_type_id = 0
 org_amt = 0
 create_time = 0
 
-
-
-
 # 平台优惠
 '''
 平台价格设置表格，油分类-油产品-省市区-油站：获取折扣价格时，条件越具体，优先级越高。
@@ -28,6 +25,8 @@ create_time = 0
    9.油产品
    10.油分类
 '''
+
+
 def platform_price(order_id):
     s = 'select site_id,product_type_id,product_category from pit_oil_order where id=' + str(order_id)
     list = ms.ExecQuery(s)
@@ -136,12 +135,12 @@ def site_price(order_id):
     for i in list:
         # discount_money_per_litre 验证加油升数是否正确
         if (i[7] == 1):
-            org_oil_litre_site=org_amt/(org_price-i[9])
-            if(org_oil_litre-org_oil_litre_site<0.0001):
+            org_oil_litre_site = org_amt / (org_price - i[9])
+            if (org_oil_litre - org_oil_litre_site < 0.0001):
                 print('加油升数正常')
             else:
-                print(org_oil_litre_site,'org_oil_litre_site')
-                print(org_oil_litre_site,'org_oil_litre_site')
+                print(org_oil_litre_site, 'org_oil_litre_site')
+                print(org_oil_litre_site, 'org_oil_litre_site')
                 exit('加油升数异常')
         # discount_fullcut
         elif (org_amt >= i[10]):
@@ -151,29 +150,62 @@ def site_price(order_id):
     return max(tmp_list)
 
 
-def member_marketing(province_id,city_id,district_id):
-    s='select * from  pit_oil_platform_activity where product_type_ids='+str(product_type_id)+\
-      ' and start_time<='+str(create_time)+' and end_time>='+str(create_time) +\
-      ' and limit_min_spend_money<='+str(org_amt)
-    list=ms.ExecQuery(s)
-    tmp_list=[]
+def member_marketing(province_id, city_id, district_id, tag_ids):
+    s = 'select * from  pit_oil_platform_activity where product_type_ids like \'%\"' + str(product_type_id) + '\"%\'' + \
+        ' and start_time<=\'' + str(create_time) + '\' and end_time>=\'' + str(create_time) + \
+        ' \'and limit_min_spend_money<=' + str(org_amt)
+    list = ms.ExecQuery(s)
+    tmp_list = [0]
+
+    # 如果有多个会员活动，取优惠最大？
     for i in list:
-        if(len(i[14])!=0 ):
-            if(province_id not in i[14]):
+        # 验证用户标签
+        j = 20
+        flag = 0
+        if ( i[20]==0 and i[21]==0 and i[22] ==0):
+            print('no tag limit')
+        elif(tag_ids is None):
+            flag=1
+        else:
+            while (j < 23):
+                if (i[j] is not None and i[j] not in tag_ids):
+                    flag = 1
+                    break
+                j += 1
+        if (flag == 1): break
+        # todo
+        if (i[14] != '0'):
+            if (province_id not in i[14]):
                 continue
-            elif(len(i[15]!=0)):
-                if(city_id not in i[15]):
+            elif (i[15] != '0'):
+                if (city_id not in i[15]):
                     continue
-                elif(len(i[17])!=0):
-                    if(district_id not in i[17]):
+                elif (i[17]!= '0'):
+                    if (district_id not in i[17]):
                         continue
-        #1立减 2折扣(如9.5折)
-        if(i[6]==1):
+        # 1立减 2折扣(如9.5折)
+        if (i[6] == 1):
             tmp_list.append(i[9])
         else:
-            money=
+            money = org_amt * i[10]
+            if (money > i[18]):
+                money = i[18]
+            tmp_list.append(money)
+    return max(tmp_list)
 
 
+def plat_site_balance(product_type_id, site_id, org_amt, org_oil_litre):
+    s = 'select * from pit_oil_site_product ' + ' where product_type_id=' + str(product_type_id) + ' and status=1'
+    print(s)
+    list = ms.ExecQuery(s)
+    tmp_list = [0]
+    for i in list:
+        # 能源 - 折扣模式(1百分比2每升减)
+        if (i[5] == 1):
+            tmp_list.append(org_amt * (1 - i[6]))
+        else:
+            tmp_list.append(org_amt - org_oil_litre * i[7])
+    return max(tmp_list)
 
 
 def order_detail(col):
@@ -182,46 +214,59 @@ def order_detail(col):
     return list[0][0]
 
 
-def com_select(table,col):
-    s='select top 1000' + col + ' from '+table
-
-    list=ms.ExecQuery(s)
-    tmp_list=[]
+def com_select(table, col):
+    s = 'select top 100 ' + col + ' from ' + table + ' order by id desc'
+    s = 'select top 10 *  from ' + table + ' where id in (' + s + ')' + ' order by id '
+    list = ms.ExecQuery(s)
+    tmp_list = []
     for i in list:
         tmp_list.append(i[0])
     return tmp_list
 
-def site_detail(col,site_id):
+
+def site_detail(col, site_id):
     s = 'select ' + col + ' from pit_drp_site where id=' + str(site_id)
     list = ms.ExecQuery(s)
     return list[0][0]
 
 
+def user_to_tag(user_id):
+    s = 'select tag_id' + ' from pit_member_user_to_tag where user_id=' + str(user_id)
+    list = ms.ExecQuery(s)
+    if (len(list) == 0):
+        return None
+    return list[0]
+
+
 if __name__ == '__main__':
     tmp_order0 = [18822826]
-    # tmp_order1 = com_select('pit_oil_order','id')
-
+    tmp_order1 = com_select('pit_oil_order', 'id')
 
     tmp_list = []
     for order_id in tmp_order0:
-        #order
+        # order
         org_price = order_detail('org_price')
         org_oil_litre = order_detail('org_oil_litre')
         site_id = order_detail('site_id')
         product_type_id = order_detail('product_type_id')
         org_amt = order_detail('org_amt')
-
-        #site
-        province_id=site_detail('province_id',site_id)
-        city_id=site_detail('city_id',site_id)
-        district_id=site_detail('district_id',site_id)
-
+        user_id = order_detail('user_id')
 
         # 修改字符，去掉多余的时间字符
-        create_time = str( order_detail('create_time')).split('.')[0]
-        dic_reduce={}
-        dic_reduce['site']=site_price(order_id)
-        dic_reduce['platform']=platform_price(order_id)
+        create_time = str(order_detail('create_time')).split('.')[0]
+
+        # site
+        province_id = site_detail('province_id', site_id)
+        city_id = site_detail('city_id', site_id)
+        district_id = site_detail('district_id', site_id)
+
+        tag_ids = user_to_tag(user_id)
+
+        dic_reduce = {}
+        dic_reduce['site'] = site_price(order_id)
+        dic_reduce['platform'] = platform_price(order_id)
+        dic_reduce['member'] = member_marketing(province_id, city_id, district_id, tag_ids)
+        dic_reduce['plat_site_balance'] = plat_site_balance(product_type_id, site_id, org_amt, org_oil_litre)
         tmp_list.append(dic_reduce)
     print('---------------------------------------')
     print(tmp_list)
--- sql_server.py
+++ sql_server.py
@@ -2,8 +2,9 @@
 
 import pymssql
 
+
 class MSSQL:
-    def __init__(self,host,user,pwd,db):
+    def __init__(self, host, user, pwd, db):
         self.host = host
         self.user = user
         self.pwd = pwd
@@ -11,28 +12,30 @@ class MSSQL:
 
     def __GetConnect(self):
         if not self.db:
-            raise(NameError,"没有设置数据库信息")
-        self.conn = pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset="utf8")
+            raise (NameError, "没有设置数据库信息")
+        self.conn = pymssql.connect(host=self.host, user=self.user, password=self.pwd, database=self.db, charset="utf8")
         cur = self.conn.cursor()
         if not cur:
-            raise(NameError,"连接数据库失败")
+            raise (NameError, "连接数据库失败")
         else:
             return cur
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         resList = cur.fetchall()
 
-        #查询完毕后必须关闭连接
+        # 查询完毕后必须关闭连接
         self.conn.close()
         return resList
 
-    def ExecNonQuery(self,sql):
+    def ExecNonQuery(self, sql):
         cur = self.__GetConnect()
         cur.execute(sql)
         self.conn.commit()
         self.conn.close()
+
+
 '''
 ms = MSSQL(host="192.168.1.1",user="sa",pwd="sa",db="testdb")
 reslist = ms.ExecQuery("select * from webuser")
@@ -43,3 +46,4 @@ newsql="update webuser set name='%s' where id=1"%u'测试'
 print(newsql)
 ms.ExecNonQuery(newsql.encode('utf-8'))
 '''
+
